02f67f74a0f6dc085b66b046e9e7ccc496ea4d98
Aayush Chatterjee <99548784+aayush8421@users.noreply.github.com>
2023-03-13 07:57:39 +0530

    day 1

---
+++ 05_Variables.php
@@ -0,0 +1,13 @@
+<?php
+
+// echo "This is video 5";
+// variables start with a $
+// variables are case sensitive but keywords are not
+
+$name = "aayush";
+$income = 5000;
+echo "My name is $name and my income is Rs.$income<br>";
+echo "$name is a very good boy<br>";
+echo "$name is a real gangsta<br>";
+
+?>
